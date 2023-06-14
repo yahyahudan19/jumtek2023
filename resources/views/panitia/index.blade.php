a8513d9ffa2fad713025e4065fa5903d262f40c5
yahyahudan19 <yahyahudan19@gmail.com>
2023-06-14 21:39:42 +0700

    Jumtek 2023 | Yok Tinggal Dikit lagi

--- resources/views/panitia/index.blade.php
+++ resources/views/panitia/index.blade.php
@@ -399,11 +399,11 @@
                 </div>
                 <div class="row">
                     <div class="col-xl-6 col-lg-6 col-sm-6">
-						<div class="widget-stat card bg-success">
+						<div class="widget-stat card bg-danger">
 							<div class="card-body  p-4">
 								<div class="media">
 									<span class="mr-3">
-										<i class="flaticon-381-plus"></i>
+										<i class="flaticon-381-home"></i>
 									</span>
 									<div class="media-body text-white text-right">
 										<p class="mb-1">Total Kontingen Pendaftar : </p>
