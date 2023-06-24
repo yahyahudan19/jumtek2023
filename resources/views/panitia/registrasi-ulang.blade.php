95e08629ab30a3aa05dc14ccd8a54c42127f98c2
yahyahudan19 <yahyahudan19@gmail.com>
2023-06-24 09:38:58 +0700

    Jumtek 2023 | Semangat Kak !!!

--- resources/views/panitia/registrasi-ulang.blade.php
+++ resources/views/panitia/registrasi-ulang.blade.php
@@ -83,7 +83,7 @@
                                                 @else
                                                 <td class="py-2 text-right"><span class="badge badge-info">Panitia</span></td>
                                                 @endif
-                                                <td>{{ \Carbon\Carbon::parse($peserta->updated_at)->format('d M Y h:i') WIB}}</td>
+                                                <td>{{ \Carbon\Carbon::parse($peserta->updated_at)->format('d M Y h:i')}} WIB</td>
                                                 <td><a href="/registrasi/null/{{$peserta->id_peserta}}" class="btn btn-danger shadow btn-xs sharp"><i class="fa fa-times"></i></a></td>
                                             </tr>
                                             @endforeach
